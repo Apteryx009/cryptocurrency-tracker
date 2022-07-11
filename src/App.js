c0b3d8c2742822404facab9855e8463f467cf5bd
Jacob Mccalip <jacobmccalip@gmail.com>
2022-07-10 22:41:07 -0500

    Added Header, CoinPage, and Homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import {BrowserRouter, Route} from 'react-router-dom'
 import "./App.css"; 
-
+import Header from './components/Header'
+import CoinPage from './Pages/CoinPage';
+import Homepage from './Pages/Homepage';
 
 function App() {
-    return <div className="App">Hello World</div>;
+    return (
+    <BrowserRouter>
+     <div>
+        <Header />
+        <Route path='/' component={Homepage} exact/>
+        <Route path='/coins/:id' component={CoinPage}/> 
+
+    </div>
+    </BrowserRouter>
+    )
 }
 
 export default App;
