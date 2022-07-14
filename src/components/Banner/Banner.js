78fd33fa99bb51f75be11c7115c914500b95990a
Chigozie Onyia <cpo21@txstate.edu>
2022-07-13 20:47:56 -0500

    Added Crypto Banner and Tagline

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,20 +1,47 @@
-import { Container, makeStyles } from "@material-ui/core";
+import { Container, makeStyles, Typography } from "@material-ui/core";
 import React from "react";
 
 const useStyles = makeStyles(() => ({
   banner: {
-    background: "url(./banner2.jpg",
+    background: "url(./banner2.jpg)",
   },
   bannerContent: {
     height: 400,
+    display: "flex",
+    flexDirection: "column",
+    paddingTop: 25,
+    justifyContent: "space-around",
   },
 }));
 
 const Banner = () => {
-  const classes = useStyles;
+  const classes = useStyles();
   return (
-    <div className={classes.Banner}>
-      <Container className={classes.bannerContent}></Container>
+    <div className={classes.banner}>
+      <Container className={classes.bannerContent}>
+        <div className={classes.tagline}>
+          <Typography
+            variant='h2'
+            style={{
+              fontWeight: "bold",
+              marginBottom: 15,
+              fontFamily: "Montserrat",
+            }}
+          >
+            Crypto Hunter
+          </Typography>
+          <Typography
+            variant='subtitle2'
+            style={{
+              fontWeight: "darkgrey",
+              textTransform: "capitalize",
+              fontFamily: "Montserrat",
+            }}
+          >
+            Get all the Info regarding your favorite Crypto Currency
+          </Typography>
+        </div>
+      </Container>
     </div>
   );
 };
