261ed6e315c2938f9a9da6bcfef2cd33b276a626
Chigozie Onyia <cpo21@txstate.edu>
2022-07-14 09:00:24 -0500

    removed duplicated import statement

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import "react-alice-carousel/lib/alice-carousel.css";
 import CryptoContext from "./CryptoContext";
-import "react-alice-carousel/lib/alice-carousel.css";
 
 ReactDOM.render(
   <CryptoContext>
