520e68562de5ffca657da7c3990f388855cd7b8d
Chigozie Onyia <cpo21@txstate.edu>
2022-07-23 10:04:06 -0500

    Added a ] header for the coins table

--- src/components/Banner/CoinsTable.js
+++ src/components/Banner/CoinsTable.js
@@ -1,80 +1,101 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { CoinList } from '../../config/api'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { CoinList } from "../../config/api";
 // Here we are implmenting the Coinlist API
 
 import { CryptoState } from "../../CryptoContext";
-import {TextField, Container, createTheme, ThemeProvider, Typography, TableContainer} from '@material-ui/core';
+import {
+  TextField,
+  Container,
+  createTheme,
+  ThemeProvider,
+  Typography,
+  TableContainer,
+  LinearProgress,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+} from "@material-ui/core";
 
 const CoinsTable = () => {
-   const [coins, setCoins] = useState([])
-   const [loading, setLoading] = useState(false)
-   const [search, setSearch] = useState()
-    
-//    Destructing currency from CryptoSTate
-    const {currency} =  CryptoState()
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState();
 
+  //    Destructing currency from CryptoSTate
+  const { currency } = CryptoState();
 
-    const fetchCoins = async () => {
-        setLoading(true)
+  const fetchCoins = async () => {
+    setLoading(true);
 
-        const {data} = await axios.get(CoinList(currency))
+    const { data } = await axios.get(CoinList(currency));
 
-        setCoins(data)
-        setLoading(false)
-    };
+    setCoins(data);
+    setLoading(false);
+  };
 
-    console.log(coins)
+  console.log(coins);
 
-    // Every time currency changes we are gonna call this use state
-    useEffect(() => {
-        fetchCoins()
-    }, [currency])
- 
+  // Every time currency changes we are gonna call this use state
+  useEffect(() => {
+    fetchCoins();
+  }, [currency]);
 
-    const darkTheme = createTheme({
-        palette: {
-          primary: {
-            main: "#fff",
-          },
-          type: "dark",
-        },
-      });
-    
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
 
- 
-   return (
-   <ThemeProvider theme={darkTheme}>
-        <Container style ={{textAlign: "center"}}>
-
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Container style={{ textAlign: "center" }}>
         <Typography
-          variant="h4"
+          variant='h4'
           style={{ margin: 18, fontFamily: "Montserrat" }}
         >
           Cryptocurrency Prices by Market Cap
         </Typography>
 
         <TextField
-          label="Search For a Crypto Currency.."
-          variant="outlined"
+          label='Search For a Crypto Currency..'
+          variant='outlined'
           style={{ marginBottom: 20, width: "100%" }}
           onChange={(e) => setSearch(e.target.value)}
         />
 
         <TableContainer>
-            {/* {
-                loading?(
-
-                ) 
-            } */}
-        
+          {loading ? (
+            <LinearProgress style={{ backgroundColor: "gold" }} />
+          ) : (
+            <Table>
+              <TableHead style={{ backgroundColor: "#EEBC1D" }}>
+                <TableRow>
+                  {["Coin", "Price", "24h Change", "Market Cap"].map((head) => (
+                    <TableCell
+                      style={{
+                        color: "black",
+                        fontWeight: "700",
+                        fontFamily: "Montserrat",
+                      }}
+                      key={head}
+                      align={head === "Coin" ? "" : "right"}
+                    >
+                      {head}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+            </Table>
+          )}
         </TableContainer>
-           
-        </Container>
-
-   </ThemeProvider>
+      </Container>
+    </ThemeProvider>
   );
-
 };
 
-export default CoinsTable
+export default CoinsTable;
