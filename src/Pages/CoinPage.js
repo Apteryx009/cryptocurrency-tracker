55c397e8bb015fd3b09caf22f78761526008ada2
Chigozie Onyia <cpo21@txstate.edu>
2022-07-23 13:40:21 -0500

    Created Coin info component for CoinPage

--- src/Pages/CoinPage.js
+++ src/Pages/CoinPage.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core";
+import { useParams } from "react-router-dom";
+import { SingleCoin } from "../config/api";
+import { CryptoState } from "../CryptoContext";
+import CoinInfo from "../components/CoinInfo";
+import axios from "axios";
 
 const CoinPage = () => {
-  return (
-    <div>
-      Coin Page
-    </div>
-  )
-}
+  const { id } = useParams();
+  const [coin, setCoin] = useState();
 
-export default CoinPage
+  const { currency, symbol } = CryptoState();
+
+  const fetchCoin = async () => {
+    const { data } = await axios.get(SingleCoin(id));
+
+    setCoin(data);
+  };
+
+  useEffect(() => {
+    fetchCoin();
+  }, []);
+
+  const useStyles = makeStyles(() => ({}));
+
+  const classes = useStyles();
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.sidebar}>{/*sidebar*/}</div>
+      {/*chart*/}
+      <CoinInfo coin={coin} />
+    </div>
+  );
+};
+
+export default CoinPage;
